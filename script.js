b94854e073ce8f74aaa3ef38382592f3177ae1eb
domesimbana <domesimbana123@gmail.com>
2025-05-13 04:47:45 -0400

    Start of main page

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("JavaScript is connected!");
+/*Location of text on the page*/
+window.onload=function () {
+    /*dome title location*/
+    const domeTitle=document.getElementById('domeTitle');
+    domeTitle.style.left='60px';
+    domeTitle.style.top='25px';
+    /*line location*/
+    const line=document.getElementById('line');
+    line.style.left='60px';
+    line.style.top='100px';
+    /*home title location*/
+    const home=document.getElementById('homeTitle');
+    home.style.left='750px';
+    home.style.top='60px';
+    /*about title location*/
+    const about=document.getElementById('aboutTitle');
+    about.style.left='975px';
+    about.style.top='60px';
+    /*contact title location*/
+    const contact=document.getElementById('contactTitle');
+    contact.style.left='1225px';
+    contact.style.top='60px';
+  };

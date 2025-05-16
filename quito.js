2c656f477f450bda7e7cc7894805b0a6ab9dfeaf
domesimbana <domesimbana123@gmail.com>
2025-05-16 03:03:41 -0400

    Ecuador page actually complete

--- quito.js
+++ quito.js
@@ -11,10 +11,23 @@ window.onload=function () {
     const home=document.getElementById('homeTitle');
     home.style.left='750px';
     home.style.top='60px';
+    home.addEventListener('click', () => {
+        home.classList.add('clicked');
+        setTimeout(() => {
+          window.location.href = 'index.html';
+        }, 300);
+      });
     /*about title location*/
     const about=document.getElementById('aboutTitle');
     about.style.left='975px';
     about.style.top='60px';
+    about.addEventListener('click', () => {
+        about.classList.add('clicked');
+        setTimeout(() => {
+          window.location.href = 'quito.html';
+        }, 300);
+      });
+
     /*contact title location*/
     const contact=document.getElementById('contactTitle');
     contact.style.left='1225px';
